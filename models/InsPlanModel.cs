7f83d0b73529e5a4fc6ea5c3282126aa573ef511
Brian Kirby <bnkirby@gmail.com>
2024-06-09 21:24:21 -0400

    Remove more bad constructors

--- models/InsPlanModel.cs
+++ models/InsPlanModel.cs
@@ -23,22 +23,5 @@ namespace InsConsole.models
         public decimal? IndividualOutOfPocketMaxIn { get; set; }
         public decimal? IndividualOutOfPocketMaxOut { get; set; }
 
-        public InsPlan(long PlanId_, decimal IndividualDeductIn_, decimal FamilyDeductIn_, decimal IndividualDeductOut_, decimal FamilyDeductOut_, decimal ReimbursementFunds_, int? CoPay_, int? CoinsuranceShareUrgentCare_, int? CoinsuranceShareExams_, int? CoinsuranceShareER_, decimal? FamilyOutOfPocketMaxIn_, decimal? FamilyOutofPocketMaxOut_, decimal? IndividualOutOfPocketMaxIn_, decimal? IndividualOutOfPocketMaxOut_)
-        {
-            this.PlanId = PlanId_;
-            this.IndividualDeductIn = IndividualDeductIn_;
-            this.FamilyDeductIn = FamilyDeductIn_;
-            this.IndividualDeductOut = IndividualDeductOut_;
-            this.FamilyDeductOut = FamilyDeductOut_;
-            this.ReimbursementFunds = ReimbursementFunds_;
-            this.CoPay = CoPay_;
-            this.CoinsuranceShareUrgentCare = CoinsuranceShareUrgentCare_;
-            this.CoinsuranceShareExams = CoinsuranceShareExams_;
-            this.CoinsuranceShareER = CoinsuranceShareER_;
-            this.FamilyOutOfPocketMaxIn = FamilyOutOfPocketMaxIn_;
-            this.FamilyOutofPocketMaxOut = FamilyOutofPocketMaxOut_;
-            this.IndividualOutOfPocketMaxIn = IndividualOutOfPocketMaxIn_;
-            this.IndividualOutOfPocketMaxOut = IndividualOutOfPocketMaxOut_;
-        }
     }
 }
--- models/VisitModel.cs
+++ models/VisitModel.cs
@@ -4,15 +4,8 @@
     {
         public long Id { get; set; }
         public string Date {get;set;}
-    public long Person { get; set; }
-    public string Provider { get; set; }
+        public long Person { get; set; }
+        public string Provider { get; set; }
 
-    public Visit(long Id_, string Date_, long Person_, string Provider_)
-    {
-        this.Id = Id_;
-        this.Date = Date_;
-        this.Person = Person_;
-        this.Provider = Provider_;
     }
-}
 }
